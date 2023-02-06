dc12219c03d4cf21393e6613c97d03bd66ebccb4
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-02-06 14:40:26 +0300

    cleaning code

--- controllers/deleteCar.js
+++ controllers/deleteCar.js
@@ -6,19 +6,16 @@ const{carId}=req.params
     try{
        
                      
-        await DeleteFromCloudinary(carId,res).then(async(_req,res) => {
+        await DeleteFromCloudinary(carId,res)
             const car = await carsModel.findOneAndDelete({"carID":carId});
         
             console.log(car)
             res.json({message:"destroyed", "data":car});
             if(!car){
                 res.json({message:`car with id ${carId} does not exist`, "data":car});
-                throw new Error(`car with id ${carId} does not exist`);
+               
             }
-        }).catch(error => {
-            console.log(error);
-            res.status(500).json({message: 'Error while deleting the car', error: error});
-          });
+      
         
        
 
